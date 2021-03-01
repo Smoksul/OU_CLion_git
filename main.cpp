18a6f018685b6341d58c0d6c48fe5f4ce69eac0f
tzela <tzelawski@gmail.com>
2021-03-01 14:12:41 +0100

    zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 Int Policz (int a, int b) {
-    Return a*b;
+    Return (a+b)*(a-b);
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std:: cout <<"W drugim branchu; 2*3=" << Policz(2,3) <<std::endl;
+    std:: cout <<"W trzecim branchu; (2+3)*(2-3)=" << Policz(2,3) <<std::endl;
     return 0;
 }
 
